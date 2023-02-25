d8ca8af42597528a82063d27dee71a07c93cbc09
Vive007 <vivekpredator007@gmail.com>
2023-02-25 19:08:37 +0530

    added public folder to make avilable static file And added css file"styles.css"

--- app.js
+++ app.js
@@ -5,38 +5,50 @@ const port=3000;
 const app=express();
 // now we use ejs templating to avoid write more html file containg almost same code.
 app.set('view engine', 'ejs');
+// make avilable static file.
+app.use(express.static("public"));
 // using boydy-parser
 app.use(bodyParser.urlencoded({extended:true}));
 
 // use a function to get today date
-const echoDay=["sunday","monday","tuesday","wednesday","Thursday","Friday","saturday"];
+//const echoDay=["sunday","monday","tuesday","wednesday","Thursday","Friday","saturday"];
 var today=new Date();
 var dayId=today.getDay();// it return integer coreespond to today day.
 var dateId=today.getDate();
-var data1=[];// creating Array to store previous data avilable in to doList.
-// formatin day and date with option
+var data1=["WELCOME","How Are you","Make your list:"];// creating Array to store previous data avilable in to doList.
+var workList=[];
+// formating day and date with option
 var options={
     weekday:"long",
     day:"numeric",
     month:"long"
 };
-var strDay=today.toDateString("en-US",options);
+var strDay=today.toDateString("en-US",options);// formating day date string.
 app.listen(port,function()
 {
     console.log("Server started at port 3000");
    // console.log(dateId);
 });
 app.get("/", (req, res) => {
-    res.render("list", {day: strDay,newItems:data1});// we use render 
+    res.render("list", {day: strDay,newItems:data1});// we use render to use ejs with html 
   });   
   app.post("/",function(req,res)
   {
+    let logi=req.body.list;
+    if(logi=="WorkDay")
+    {
+      workList.push(req.body.item);
+      res.redirect("/work");
+    }else{
+    //console.log(req.body);
     data1.push(req.body.item);
     res.redirect("/");
+    }// we dont use render because previour data get lost.
   });                       
-// app.get("/",function(req,res)
-// {
-//     res.write("working");
-//     res.write(echoDay[ dayId]);
-//     res.send();
-// });
+ app.get("/work",function(req,res)
+{
+  //   res.write("working");
+  //  // res.write(echoDay[ dayId]);
+  //   res.send();
+  res.render("list", {day:"WorkDay",newItems:workList});
+});
